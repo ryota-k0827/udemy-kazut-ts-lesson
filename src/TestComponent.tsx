74bd1df234dcccde76e6302d9a391d227ba5fc95
ryota-k0827 <ryota171124@icloud.com>
2022-04-14 03:04:00 +0900

    react hooks usestate

--- src/TestComponent.tsx
+++ src/TestComponent.tsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 interface Props {
   text: string;
 }
+interface UserData {
+  id: number;
+  name: string;
+}
 
 const TestComponent: React.FC<Props> = (props) => {
+  const [count, setCount] = useState<number | null>(null);
+  const [user, setUser] = useState<UserData>({ id: 1, name: "Daniel" });
+
   return (
     <div>
       <h1>{props.text}</h1>
+      <h2>{count}</h2>
     </div>
   );
 };
