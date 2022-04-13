e632ff4bcc601ebc1103ffa5fd2cf18bac70c21c
ryota-k0827 <ryota171124@icloud.com>
2022-04-14 03:13:03 +0900

    event handler

--- src/TestComponent.tsx
+++ src/TestComponent.tsx
@@ -11,11 +11,18 @@ interface UserData {
 const TestComponent: React.FC<Props> = (props) => {
   const [count, setCount] = useState<number | null>(null);
   const [user, setUser] = useState<UserData>({ id: 1, name: "Daniel" });
+  const [inputData, setInputData] = useState<string>("");
+
+  const onChangeInputData = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputData(e.target.value);
+  };
 
   return (
     <div>
       <h1>{props.text}</h1>
       <h2>{count}</h2>
+      <input type="text" value={inputData} onChange={onChangeInputData} />
+      <h1>{inputData}</h1>
     </div>
   );
 };
